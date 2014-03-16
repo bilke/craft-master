0302701c56c6705627c7ccfbcb4b179e2a7be2e5
Lars Bilke <lars.bilke@ufz.de>
2014-03-16 11:17:15 +0100

    Added breakpoint.

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ gem 'mysql'
 # Add your project gems here
 gem 'compass', "~> 1.0.0.alpha.19"
 gem 'susy'
+gem 'breakpoint'
--- public/config.rb
+++ public/config.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require "susy"
+require "breakpoint"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
